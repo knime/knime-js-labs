ed6c8c7b7c2fdaf7724b9c7509f6163297c5795b
Christian Albrecht <christian.albrecht@knime.com>
2017-12-01 00:00:07 -0800

    AP-8450: Sample implementation for Table Editor
    AP-8450: Javascript views don't save the view changes, if inside of a wrapped metanode

--- org.knime.js.views.labs/src/org/knime/js/base/node/viz/tableeditor/TableEditorViewNodeModel.java
+++ org.knime.js.views.labs/src/org/knime/js/base/node/viz/tableeditor/TableEditorViewNodeModel.java
@@ -413,15 +413,19 @@ public class TableEditorViewNodeModel extends AbstractWizardNodeModel<TableEdito
             //added with 3.3
             viewRepresentation.setDisplayFullscreenButton(m_config.getDisplayFullscreenButton());
             viewRepresentation.setEnableHideUnselected(m_config.getEnableHideUnselected());
-            viewValue.setPublishSelection(m_config.getPublishSelection());
-            viewValue.setSubscribeSelection(m_config.getSubscribeSelection());
-            viewValue.setPublishFilter(m_config.getPublishFilter());
-            viewValue.setSubscribeFilter(m_config.getSubscribeFilter());
+            if (isViewValueEmpty()) {
+                viewValue.setPublishSelection(m_config.getPublishSelection());
+                viewValue.setSubscribeSelection(m_config.getSubscribeSelection());
+                viewValue.setPublishFilter(m_config.getPublishFilter());
+                viewValue.setSubscribeFilter(m_config.getSubscribeFilter());
+            }
 
             //added with 3.4
             viewRepresentation.setDisplayMissingValueAsQuestionMark(m_config.getDisplayMissingValueAsQuestionMark());
             viewRepresentation.setDateTimeFormats(m_config.getDateTimeFormats().getJSONSerializableObject());
-            viewValue.setHideUnselected(m_config.getHideUnselected() && !m_config.getSingleSelection());
+            if (isViewValueEmpty()) {
+                viewValue.setHideUnselected(m_config.getHideUnselected() && !m_config.getSingleSelection());
+            }
 
             //added with 3.5
             viewRepresentation.setSingleSelection(m_config.getSingleSelection());
@@ -429,8 +433,10 @@ public class TableEditorViewNodeModel extends AbstractWizardNodeModel<TableEdito
 
             //editor settings
             viewRepresentation.setEditableColumns(m_config.getEditableColumnFilterConfig().applyTo(m_table.getDataTableSpec()).getIncludes());
-            viewValue.setEditorChanges(m_config.getEditorChanges());
-            viewValue.setTableHash(m_config.getTableHash());
+            if (isViewValueEmpty()) {
+                viewValue.setEditorChanges(m_config.getEditorChanges());
+                viewValue.setTableHash(m_config.getTableHash());
+            }
         }
     }
 
