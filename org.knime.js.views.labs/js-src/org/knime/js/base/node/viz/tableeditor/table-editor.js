0376e2b818294937afbcfab1a8ea7c345f4ff1fa
Oleg Yasnev <oleg.yasnev@knime.com>
2018-01-16 11:44:02 +0100

    AP-7762: Added a corresponding check and formatting
    AP-7762 (Minus zero in Table View (JavaScript) and Table Editor
    (JavaScript) numeric formatters)

--- org.knime.js.views.labs/js-src/org/knime/js/base/node/viz/tableeditor/table-editor.js
+++ org.knime.js.views.labs/js-src/org/knime/js/base/node/viz/tableeditor/table-editor.js
@@ -381,7 +381,10 @@ table_editor = function() {
 							if (!$.isNumeric(data)) {
 								return data;
 							}
-							return Number(data).toFixed(_representation.globalNumberFormatDecimals);
+							var precision = _representation.globalNumberFormatDecimals;
+							// to prevent "minus zero" (-0 or -0.0)
+							var toFixed = Math.abs(data) < Math.pow(10, -precision) ? (0).toFixed(precision) : data.toFixed(precision);
+							return toFixed;
 						}
 					}
 				}
