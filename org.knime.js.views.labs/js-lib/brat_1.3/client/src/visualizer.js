1531454954ea562201437d9cb9e5fd85bface17e
Daniel Bogenrieder <daniel.bogenrieder@knime.com>
2019-04-25 15:20:23 +0200

    AP-9486: Replaced tabs with spaces
    AP-9486 (Document Viewer (JavaScript))

--- org.knime.js.views.labs/js-lib/brat_1.3/client/src/visualizer.js
+++ org.knime.js.views.labs/js-lib/brat_1.3/client/src/visualizer.js
@@ -1744,15 +1744,15 @@ Util.profileStart('chunks');
           var lastRow = row;
 
           if (chunk.sentence) {
-	          while (sentenceNumber < chunk.sentence) {
-	            sentenceNumber++;
-	            row.arcs = svg.group(row.group, { 'class': 'arcs' });
-	            rows.push(row);
-	            row = new Row(svg);
-	            sentenceToggle = 1 - sentenceToggle;
-	            row.backgroundIndex = sentenceToggle;
-	            row.index = ++rowIndex;
-	          }
+            while (sentenceNumber < chunk.sentence) {
+              sentenceNumber++;
+              row.arcs = svg.group(row.group, { 'class': 'arcs' });
+              rows.push(row);
+              row = new Row(svg);
+              sentenceToggle = 1 - sentenceToggle;
+              row.backgroundIndex = sentenceToggle;
+              row.index = ++rowIndex;
+            }
             sentenceToggle = 1 - sentenceToggle;
           }
 
@@ -2372,7 +2372,7 @@ Util.profileStart('rows');
         // position the rows
         var y = Configuration.visual.margin.y;
         if (showLineNumber) {
-        	var sentNumGroup = svg.group({'class': 'sentnum'});
+          var sentNumGroup = svg.group({'class': 'sentnum'});
         }
         var currentSent;
         $.each(rows, function(rowId, row) {
@@ -2425,32 +2425,32 @@ Util.profileStart('rows');
           y += sizes.texts.height;
           row.textY = y - rowPadding;
           if (showLineNumber) {
-	          if (row.sentence) {
-	            var sentence_hash = new URLHash(coll, doc, { focus: [[ 'sent', row.sentence ]] } );
-	            var link = svg.link(sentNumGroup, sentence_hash.getHash());
-	            var text = svg.text(link, sentNumMargin - Configuration.visual.margin.x, y - rowPadding,
-	                '' + row.sentence, { 'data-sent': row.sentence });
-	            var sentComment = data.sentComment[row.sentence];
-	            if (sentComment) {
-	              var box = text.getBBox();
-	              svg.remove(text);
-	              // TODO: using rectShadowSize, but this shadow should
-	              // probably have its own setting for shadow size
-	              shadowRect = svg.rect(sentNumGroup,
-	                box.x - rectShadowSize, box.y - rectShadowSize,
-	                box.width + 2 * rectShadowSize, box.height + 2 * rectShadowSize, 
-	                {
-	                  'class': 'shadow_' + sentComment.type,
-	                  filter: 'url(#Gaussian_Blur)',
-	                  rx: rectShadowRounding,
-	                  ry: rectShadowRounding,
-	                  'data-sent': row.sentence,
-	                }
-	              );
-	              var text = svg.text(sentNumGroup, sentNumMargin - Configuration.visual.margin.x, y - rowPadding,
-	                  '' + row.sentence, { 'data-sent': row.sentence });
-	            }
-	          }
+              if (row.sentence) {
+                var sentence_hash = new URLHash(coll, doc, { focus: [[ 'sent', row.sentence ]] } );
+                var link = svg.link(sentNumGroup, sentence_hash.getHash());
+                var text = svg.text(link, sentNumMargin - Configuration.visual.margin.x, y - rowPadding,
+                    '' + row.sentence, { 'data-sent': row.sentence });
+                var sentComment = data.sentComment[row.sentence];
+                if (sentComment) {
+                  var box = text.getBBox();
+                  svg.remove(text);
+                  // TODO: using rectShadowSize, but this shadow should
+                  // probably have its own setting for shadow size
+                  shadowRect = svg.rect(sentNumGroup,
+                    box.x - rectShadowSize, box.y - rectShadowSize,
+                    box.width + 2 * rectShadowSize, box.height + 2 * rectShadowSize,
+                    {
+                      'class': 'shadow_' + sentComment.type,
+                      filter: 'url(#Gaussian_Blur)',
+                      rx: rectShadowRounding,
+                      ry: rectShadowRounding,
+                      'data-sent': row.sentence,
+                    }
+                  );
+                  var text = svg.text(sentNumGroup, sentNumMargin - Configuration.visual.margin.x, y - rowPadding,
+                      '' + row.sentence, { 'data-sent': row.sentence });
+                }
+              }
           }
 
           var rowY = y - rowPadding;
